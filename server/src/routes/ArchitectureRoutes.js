a81feeaced73fd1a768857674c94f4cd4db63ff1
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-06-09 18:09:52 +0530

    feat: added architecture routes for calling architecture controllers

---
+++ server/src/routes/ArchitectureRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import {
+    forkArchitecture,
+    deleteArchitecutre,
+    createArchitecture,
+    editArchitecture,
+    getArchitectureById,
+    getArchitectures,
+    getPublicArchitectures
+    
+} from '../controllers/ArchitecturesController.js'
+import { authenticate, optionalAuth } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+//get routes
+router.get('/' , authenticate , getArchitectures)
+router.get('/public', optionalAuth, getPublicArchitectures)
+router.get('/:id', optionalAuth , getArchitectureById)
+
+//post routes
+router.post('/', authenticate , createArchitecture)
+router.post('/:id/fork', authenticate , forkArchitecture)
+
+//put route
+router.put('/:id', authenticate, editArchitecture)
+
+//delete route
+router.delete('/:id' , authenticate , deleteArchitecutre)
+
+
+
+export default router
