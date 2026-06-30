a4bcad8bbddd4ea629f5107cd0d195170c8f2f71
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-06-30 13:59:16 +0530

    refactor: simplify password hashing middleware by removing next() callback

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -32,10 +32,10 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('passwordHash')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('passwordHash')) return ;
   this.passwordHash = await bcrypt.hash(this.passwordHash, 12);
-  next();
+  
 });
 
 /**
